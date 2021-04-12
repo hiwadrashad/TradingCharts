82cf1a0cf70fb7dbc1bbd11dc0ce3b209209d99b
hiwad rashad <hiwadrashad112@hotmail.com>
2021-04-12 14:45:18 +0200

    Add project files.

---
+++ TradingCharts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TradingCharts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
